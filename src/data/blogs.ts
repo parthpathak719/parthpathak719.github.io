db2efffe07d766e3525b794871b8c51a3a308020
parthpathak719 <bl.en.u4cse23036@bl.students.amrita.edu>
2026-03-22 03:24:16 +0530

    boom

--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -2,21 +2,21 @@ export const BLOGS = [
   {
     title: "Building My Portfolio",
     description: "How I designed and built this website.",
-    link: "/Personal-Website/blog/building-my-portfolio"
+    link: "/blog/building-my-portfolio"
   },
   {
     title: "Black Box Testing On a Banking App",
     description: "My experience testing a simple banking application.",
-    link: "/Personal-Website/blog/testing-a-banking-app"
+    link: "/blog/testing-a-banking-app"
   },
   {
     title: "Collapse",
     description: "Four students. One classroom. A reality that doesn't follow the rules anymore.",
-    link: "/Personal-Website/blog/collapse"
+    link: "/blog/collapse"
   },
   {
     title: "Why One Piece Feels Bigger Than Ever Right Now",
     description: "From manga to live action, One Piece is doing something right now that's hard to ignore.",
-    link: "/Personal-Website/blog/one-piece"
+    link: "/blog/one-piece"
   }  
 ];
