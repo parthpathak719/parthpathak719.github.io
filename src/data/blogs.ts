557b2f39508f737474af8d42581984c686a1c21f
parthpathak719 <bl.en.u4cse23036@bl.students.amrita.edu>
2026-03-22 01:53:59 +0530

    done

---
+++ src/data/blogs.ts
@@ -0,0 +1,22 @@
+export const BLOGS = [
+  {
+    title: "Building My Portfolio",
+    description: "How I designed and built this website.",
+    link: "/blog/building-my-portfolio"
+  },
+  {
+    title: "Black Box Testing On a Banking App",
+    description: "My experience testing a simple banking application.",
+    link: "/blog/testing-a-banking-app"
+  },
+  {
+    title: "Collapse",
+    description: "Four students. One classroom. A reality that doesn't follow the rules anymore.",
+    link: "/blog/collapse"
+  },
+  {
+    title: "Why One Piece Feels Bigger Than Ever Right Now",
+    description: "From manga to live action, One Piece is doing something right now that's hard to ignore.",
+    link: "/blog/one-piece"
+  }  
+];
